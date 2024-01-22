23c4311f82c08773abb33acceecb72275aa4bd61
L344S <7474@holbertonstudents.com>
2024-01-22 20:25:25 +0100

    Added 2-print_alphabet.py and updated test.py

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(97, 123):  # 97 is 'a' and 123 is 'z'
+    i = chr(i)  # convert int to char
+    print(i, end='')  # (end='') print without new line
--- python-if_else_loops_functions/test.py
+++ python-if_else_loops_functions/test.py
@@ -5,3 +5,16 @@ number = 24
 print(f"You're {number} years old.")
 # or
 print(number, "is your age.")
+
+# Get the last digit of a number (positive or negative)
+number = 24
+last_digit = number % 10  # return last digit of + number
+last_digit = -(number % -10)  # return last digit of - number
+
+# Print a suite of elements :
+for i in range(0, 10):  # print 0 to 9
+    print(i)
+
+for i in range(97, 123):  # print a to z
+    i = chr(i)  # convert int to char
+    print(i, end='')    # (end='') print without new line
