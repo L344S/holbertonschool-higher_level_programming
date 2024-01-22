655b27913faf0a136e6f6831ecaecb5081dc4536
L344S <7474@holbertonstudents.com>
2024-01-22 13:01:30 +0100

    Created test.py to try files and keep notes

---
+++ python-hello_world/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Variable declaration :
+number = 24
+
+# Print a basic string :
+print("Hello World !")
+
+# Print a string with a variable without casting (using f-string) :
+print(f"You're {number} years old")
