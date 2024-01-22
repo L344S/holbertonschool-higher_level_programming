cb4f12cf91179e2ef4c0cd76b634eb9668519285
L344S <7474@holbertonstudents.com>
2024-01-22 13:19:54 +0100

    Updated test.py

--- python-hello_world/test.py
+++ python-hello_world/test.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-# Variable declaration :
-number = 24
-pi = 3.14159
-
 # Print a basic string :
 print("Hello World !")
 
 # Print a string with a variable without casting (using f-string) :
-print(f"You're {number} years old")
+number = 24
+print(f"You're {number} years old.")
 
 # Print a float with a variable without casting (using f-string) :
+pi = 3.14159
 print(f"Pie value is approximately {pi:.2f}")
+
+# Print the concatenation of two strings :
+string1 = "Holberton"
+string2 = "School"
+concatenation = string1 + " " + string2
+print(f"Welcome to {concatenation} !")
