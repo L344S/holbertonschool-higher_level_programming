f09b6a9d7000a7b183913c356861c4614cb5ecfe
L344S <7474@holbertonstudents.com>
2024-01-24 00:49:16 +0100

    Attempt to fix 9-print_last_digit.py
    REMOVE COMMENTS

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-def print_last_digit(number):  # Function that prints the last digit of a num
-    if (number >= 0):  # If number is positive
-        last = number % 10  # return last digit of + number
-    else:  # If number is negative
-        last = abs(number) % 10  # return last digit of negative number
-    print(last, end="")  # Print last digit
-    return last  # Return last digit
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number >= 0):
+        last = number % 10
+    else:
+        last = abs(number) % 10
+    print(last, end="")
+    return last
