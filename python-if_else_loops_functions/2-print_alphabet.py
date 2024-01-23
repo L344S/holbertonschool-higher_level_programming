1d1c4114f86cee0dd4327b29954924ff7c63a3ad
L344S <7474@holbertonstudents.com>
2024-01-23 21:27:31 +0100

    Attempt to fix 2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # 97 is 'a' and 123 is 'z'
-    i = chr(i)  # convert int to char
-    print(i, end='')  # (end='') print without new line
+    print(chr(i), end="")  # print each letter from a to z
