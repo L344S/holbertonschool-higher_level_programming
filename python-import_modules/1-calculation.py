b63d4050e2907dbc2b529284a6b6f510ec6946d9
L344S <7474@holbertonstudents.com>
2024-01-23 22:08:47 +0100

    Attempt 3 to fix 1-calculation.py
    import

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import calculator_1
-
-a = 10
-b = 5
-
-print(a, "+", b, "=", calculator_1.add(a, b))
-print(a, "-", b, "=", calculator_1.sub(a, b))
-print(a, "*", b, "=", calculator_1.mul(a, b))
-print(a, "/", b, "=", calculator_1.div(a, b))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(a, "+", b, "=", calculator_1.add(a, b))
+    print(a, "-", b, "=", calculator_1.sub(a, b))
+    print(a, "*", b, "=", calculator_1.mul(a, b))
+    print(a, "/", b, "=", calculator_1.div(a, b))
