1c21cb8b17384dc6f8ee7ec64ffd71b6b3c6cd63
L344S <7474@holbertonstudents.com>
2024-01-24 00:14:22 +0100

    Attempt to fix 2-print_alphabet.py
    REMOVE COMMENTS

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):  # 97 is 'a' and 123 is 'z'
-    print(chr(i), end="")  # print each letter from a to z
+for i in range(97, 123):
+    print(chr(i), end="")
