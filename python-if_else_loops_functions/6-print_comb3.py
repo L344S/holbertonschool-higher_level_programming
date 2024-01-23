b02c9f7c3aeb569603427d8224f6ebd062a1154d
L344S <7474@holbertonstudents.com>
2024-01-24 00:37:06 +0100

    Attempt to fix 6-print_comb3.py (RC + FS)

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):  # count from 0 to 9
-    for j in range(i + 1, 10):  # count from i + 1 to 9
-        if i == 8:  # if i is not 8 (last two digits combination)
-            print(f"{i}{j}")  # print each two unique digits combination
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print('{}{}'.format(i, j))
         else:
-            print(f"{i}{j}", end=", ")  # print each two unique digits comb
+            print('{}{}'.format(i, j), end=', ')
