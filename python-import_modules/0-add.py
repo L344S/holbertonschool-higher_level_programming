c5c70443250e187722fb6c24e0d2e01344fa13a8
L344S <7474@holbertonstudents.com>
2024-01-23 21:48:37 +0100

    Attempt 2 to fix 0-add.py
    import problem

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":  # makes sure the code is not executed when imported
     a = 1
     b = 2
-    print(f"{a} + {b} = {add_0.add(a, b)}")  # prints the sum of a and b
+    print(f"{a} + {b} = {add(a, b)}")  # prints the sum of a and b
