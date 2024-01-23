b0f272d0e0e0871d74d796fece779e62ba69f4a1
L344S <7474@holbertonstudents.com>
2024-01-24 00:51:02 +0100

    Attempt to fix 8-uppercase.py
    REMOVE COMMENTS

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    newStr = ""  # initialize newStr to empty string
-    for i in str:  # for loop through the string
-        ascii = ord(i)  # convert str to ascii value number
-        if ascii > 96 and ascii < 123:  # if between 'a' & 'z' means lowercase
-            ascii = ascii - 32  # convert to uppercase by subtracting 32
-            # convert ascii to char back and add to newStr
+    newStr = ""
+    for i in str:
+        ascii = ord(i)
+        if ascii > 96 and ascii < 123:
+            ascii = ascii - 32
             newStr = newStr + chr(ascii)
-        else:  # means i is already uppercase character
-            newStr = newStr + i  # add i to newStr
+        else:
+            newStr = newStr + i
     print(newStr)
