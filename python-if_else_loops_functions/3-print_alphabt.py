f5ba9e783fdab4c9d20f71aaae9a5251b63bb575
L344S <7474@holbertonstudents.com>
2024-01-23 21:30:34 +0100

    Attempt to fix 3-print_alphabt.py and remove empty line from 5-print_comb2.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # print alphabet
-    i = chr(i)  # convert ascii int to char (ex : 97 to 'a')
-    if i not in 'qe':  # if i (letter) is not 'q' or 'e'
-        print(i, end='')  # print i without new line
+    if i != 101 or i != 113:  # skip 'e' and 'q'
+        print(chr(i), end="")  # print each letter from a to z
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,3 @@ for i in range(0, 100):  # count from 0 to 99
         print(i)  # print i without new line and comma
     else:  # if i is not 99
         print(f"{i:02d}", end=', ')  # print i with 2 digits, comma and space
-
