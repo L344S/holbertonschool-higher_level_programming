4f04f24fbfd9fea7760e76f24d98bcbe5ec1e37b
L344S <7474@holbertonstudents.com>
2024-01-23 22:28:41 +0100

    Attempt 1 to fix 4-hidden_discovery.py
    import

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-
 # can't test locally hidden_4 contains null bytes + wrong version of python
-if __name__ == "__main__":  # make sure code does not execute when imported
-    import hidden_4  # import hidden_4 module
-    for i in dir(hidden_4):  # for loop through the list of names
-        print(i)  # print the names
+import hidden_4
+if __name__ == "__main__":  # makes sure the code is not executed when imported
+    # for loop through the names in hidden_4
+    # # dir() returns a list of the names of the functions, variables etc
+    for name in dir(hidden_4):
+        if ("__" != name[:2]):  # if the name does not start with __
+            print(name)  # print the name
