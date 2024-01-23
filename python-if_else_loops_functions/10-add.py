8244062474617d209a7dc77271f6c0368b3265d7
L344S <7474@holbertonstudents.com>
2024-01-23 20:54:21 +0100

    Attempt to fix 10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,4 +1,3 @@
 #!/ust/bin/python3
 def add(a, b):
-    sum = a + b  # add a and b and store the result in sum
-    return (sum)  # return the value stored in sum
+    return a + b  # return the addition of a and b
