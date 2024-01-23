dc45ff30325449ba15746ff1d5f69ca004a897e2
L344S <7474@holbertonstudents.com>
2024-01-24 00:21:42 +0100

    Attempt to fix 3-print_alphabt.py (FS)
    UPDATED PRINT FUNCTION TO USE FORMAT() STRINGS

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # print alphabet
     if i != 101 or i != 113:  # skip 'e' and 'q'
-        print(chr(i), end="")  # print each letter from a to z
+        print("{}".format(chr(i)), end="")  # print each letter from a to z
