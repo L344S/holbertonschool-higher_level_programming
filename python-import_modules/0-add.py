b4f792b8f9e9d44f90f87928e039093fba8155a7
L344S <7474@holbertonstudents.com>
2024-01-23 23:26:32 +0100

    Attempt 2 to fix 0-add.py
    ÇA N'A AUCUN SENS SI C'EST ÇA

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ from add_0 import add  # imports add function from add_0 module
 if __name__ == "__main__":  # makes sure the code is not executed when imported
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")  # prints the sum of a and b
+    print("{} + {} = {}".format(a, b, add(a, b)))  # prints the sum of a and b
