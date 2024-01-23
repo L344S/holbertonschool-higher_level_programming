f0b233e7aad068eb8c743b4bfa059c04cec71095
L344S <7474@holbertonstudents.com>
2024-01-23 21:36:18 +0100

    Attempt to fix 4-print_hexa.py

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):  # count from 0 to 98
-    print(i, "=", hex(i))  # print i and its hex value
+    print(f"{i} = {hex(i)}")  # print i and its hexadecimal value
