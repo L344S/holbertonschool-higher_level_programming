d4145f227ce13486c52e2dfb011cdf0931ce3481
L344S <7474@holbertonstudents.com>
2024-01-24 00:33:43 +0100

    Attempt to fix 5-print_comb2.py (RC + FS)

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):  # count from 0 to 99
-    if i == 99:  # if i is 99
-        print(i)  # print i without new line and comma
-    else:  # if i is not 99
-        print(f"{i:02d}", end=', ')  # print i with 2 digits, comma and space
+for i in range(0, 100):
+    if i == 99:
+        print(i)
+    else:
+        print("{:02d}".format(i), end=", ")
