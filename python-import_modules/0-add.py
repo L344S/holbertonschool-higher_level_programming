fe211c2830002f907a411d3714c84de54d754483
L344S <7474@holbertonstudents.com>
2024-01-23 21:45:16 +0100

    Attempt to fix 0-add.py
    forgot the avoid the execution when imported

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import add_0
-
-a = 1
-b = 2
-
-print(a, "+", b, "=", add_0.add(a, b))
+if __name__ == "__main__":  # makes sure the code is not executed when imported
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add_0.add(a, b)}")  # prints the sum of a and b
