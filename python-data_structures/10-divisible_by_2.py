37f4bf0fafd99c2f977fe9b040a4c9adaad8bd4e
L344S <7474@holbertonstudents.com>
2024-01-30 13:42:49 +0100

    Implemented divisible_by_2 function

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = [False] * len(my_list)
+    for i in my_list:
+        if (i % 2) == 0:
+            new_list[i] = True
+    return new_list
