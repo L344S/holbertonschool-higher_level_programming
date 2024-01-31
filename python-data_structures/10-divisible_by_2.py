ae3be7aed106baafba015416a46cd92aa46567bd
L344S <7474@holbertonstudents.com>
2024-01-31 20:39:33 +0100

    Attempt 1 to fix divisible_by_2 function (IDX)

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,5 +3,5 @@ def divisible_by_2(my_list=[]):
     new_list = [False] * len(my_list)
     for i in my_list:
         if (i % 2) == 0:
-            new_list[i] = True
+            new_list[my_list.index(i)] = True
     return new_list
