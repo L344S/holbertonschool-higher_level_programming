07410c7f88df2458e30c30efea30dce766430efa
L344S <7474@holbertonstudents.com>
2024-02-06 09:02:47 +0100

    Implemented 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """A class to define a square."""
+    def __init__(self, size):
+        """Initializes the square with the given parameter size.
+
+        Args:
+        size (int): The size of the square given as an integer.
+        """
+        self.__size = size
