d5206f1471f8190a923f9545194d7fd9085d4d7d
L344S <7474@holbertonstudents.com>
2024-02-06 09:31:11 +0100

    Implemented 3-square.py
    LOCAL OK | PYCS OK | GDS OK

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    """A class to define a square."""
+    def __init__(self, size=0):
+        """
+        Description:
+            Initializes the square with the given parameter size
+            and checks if it is not less than 0 or not an integer.
+
+        Args:
+            size (int): The size of the square given as an integer.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Description:
+            Calculates the area of the square (size * size) and returns it.
+
+        Returns:
+            The area of the square (int)
+        """
+        return self.__size * self.__size
