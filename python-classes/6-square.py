f4a96a4e83b9ab5d3424487f4b25b87454e47bf8
L344S <7474@holbertonstudents.com>
2024-02-07 20:33:56 +0100

    Attempt 2 to fix T6 (my_print)
    LOCAL OK | PYCS OK | GDS OK

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -95,10 +95,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for a in range(self.__position[1]):
+            for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0], end="")
+                for j in range(self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
