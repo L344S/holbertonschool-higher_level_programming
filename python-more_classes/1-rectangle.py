22224f0786fbf12040a55d7e5b9a6505b6183bf9
L344S <7474@holbertonstudents.com>
2024-02-07 20:47:27 +0100

    Attempt 2 to fix T1 (init)
    LOCAL OK | PYCS OK | GDS OK

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
             TypeError: If height is not an integer.
             ValueError: If height is less than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -70,7 +70,7 @@ class Rectangle:
             TypeError: If width is not an integer.
             ValueError: If width is less than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
