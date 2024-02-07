cc0f3af0d1ad0550ba3c6eab26ce20345a460613
L344S <7474@holbertonstudents.com>
2024-02-07 20:46:09 +0100

    Attempt 2 to fix T1 (init)
    LOCAL OK | PYCS OK | GDS OK

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
             width (int): The width of the rectangle given as an integer.
             height (int): The height of the rectangle given as an integer.
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
