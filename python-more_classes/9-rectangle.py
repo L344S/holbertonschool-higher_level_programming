25611782578043d04e3d1c97c309d15b3e9ee6e6
L344S <7474@holbertonstudents.com>
2024-02-07 21:31:21 +0100

    Attempt 24556 to fix T9 (setter)
    LOCAL OK | PYCS OK | GDS OK

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -75,12 +75,12 @@ class Rectangle:
             TypeError: If width is not an integer.
             ValueError: If width is less than 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
+
 
     def area(self):
         """
