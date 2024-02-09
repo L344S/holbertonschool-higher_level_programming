b0b387652b8030dd620280adde2143a8e7cf58b5
L344S <7474@holbertonstudents.com>
2024-02-09 12:03:13 +0100

    Implemented Unittest TF 6-max_integer_test.py
    LOCAL OK | PYCS OK | GDS OK

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -19,37 +19,43 @@ class TestMaxInteger(unittest.TestCase):
         test_list_is_empty
     """
     def test_max_at_end(self):
-        """Description:
+        """
+        Description:
             If max integer at the end of the list exists
         """
         self.assertEqual(max_integer([1, 2, 3]), 3)
 
     def test_max_at_beginning(self):
-        """Description:
+        """
+        Description:
             If max integer at the beginning of the list exists
         """
         self.assertEqual(max_integer([3, 2, 1]), 3)
 
     def test_max_in_middle(self):
-        """Description:
+        """
+        Description:
             If max integer in the middle of the list exists
         """
         self.assertEqual(max_integer([1, 3, 2]), 3)
 
     def test_one_negative_number(self):
-        """Description:
+        """
+        Description:
             If only one negative number exists
         """
         self.assertEqual(max_integer([1, -2, 3]), 3)
 
     def test_only_negative_numbers(self):
-        """Description:
+        """
+        Description:
             If only negative numbers exist
         """
         self.assertEqual(max_integer([-1, -2, -3]), -1)
 
     def test_list_of_one_element(self):
-        """Description:
+        """
+        Description:
             If list has only one element
         """
         self.assertEqual(max_integer([1]), 1)
