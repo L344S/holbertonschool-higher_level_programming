ed51054ab1ad37c22b352ef20c007986641f296a
L344S <7474@holbertonstudents.com>
2024-02-13 16:16:12 +0100

    Implemented 8-rectangle.py
    LOC OK | PYCS OK | GDS OK

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for Rectangle class inheriting from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """A class to define a rectangle"""
+
+    def __init__(self, width, height):
+        """
+        Description:
+            A function that initializes the instance of the class with width and height
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.integer_validator("self.__width", width)
+        self.integer_validator("self.__height", height)
+        self.__width = width
+        self.__height = height
