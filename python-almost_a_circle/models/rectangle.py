3695360550335ea18c1cfddc6ee267f144da8f13
L344S <7474@holbertonstudents.com>
2024-02-19 18:26:15 +0100

    Implemented models/rectangle.py (Tasks 9: update again method)
    LOC OK | PYCS OK | GDS

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -166,21 +166,36 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         Description:
             Updates the values of the rectangle with the given arguments.
             If args is not empty, the first argument is the id,
             the second is the width, the third is the height,
             the fourth is the x, and the fifth is the y.
+
+            If args is empty or doesn't, the arguments are passed as kwargs.
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                if key == "width":
+                    self.__width = kwargs[key]
+                if key == "height":
+                    self.__height = kwargs[key]
+                if key == "x":
+                    self.__x = kwargs[key]
+                if key == "y":
+                    self.__y = kwargs[key]
