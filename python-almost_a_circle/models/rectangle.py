dfe2a0930c34d9280a45fe053a98ba7073b70b9f
L344S <7474@holbertonstudents.com>
2024-02-19 16:33:56 +0100

    Implemented models/rectangle.py (Tasks 3 : fix error message (>=))
    LOC OK | PYCS OK | GDS

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,7 +99,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
@@ -131,6 +131,6 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
