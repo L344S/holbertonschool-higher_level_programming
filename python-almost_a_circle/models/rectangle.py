759ad9bff74b181784cd266a585c53c914d33751
L344S <7474@holbertonstudents.com>
2024-02-19 18:01:59 +0100

    Implemented models/rectangle.py (Tasks 7: update method)
    LOC OK | PYCS OK | GDS

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -165,3 +165,22 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """
+        Description:
+            Updates the values of the rectangle.
+            If args is not empty, the first argument is the id,
+            the second is the width, the third is the height,
+            the fourth is the x, and the fifth is the y.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
