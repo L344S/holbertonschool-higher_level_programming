639b460b8e8e946e21e5542ca3506df6a939ac76
L344S <7474@holbertonstudents.com>
2024-02-21 12:38:31 +0100

    Implemented Nico's unittest
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,19 +1,57 @@
 #!/usr/bin/python3
-"""Test for Base class"""
 import unittest
+import json
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    """Tests for the Base class"""
 
-    def test_id(self):
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
-        base2 = Base()
-        self.assertEqual(base2.id, 2)
-        base3 = Base(125)
-        self.assertEqual(base3.id, 125)
+    def test_assign_id(self):
+        test = Base(50)
+        self.assertEqual(test.id, 50)
+
+    def test_no_id_assigned(self):
+        test = Base()
+        self.assertEqual(test.id, 1)
+
+    def test_to_json_string(self):
+        dictionary = {'id': 50,
+                      'width': 10,
+                      'height': 5,
+                      'x': 2,
+                      'y': 3}
+        json_string = Base.to_json_string(dictionary)
+        self.assertTrue(isinstance(json_string, str))
+
+    def test_to_json_string_content(self):
+        dictionary = {'id': 50,
+                      'width': 10,
+                      'height': 5,
+                      'x': 2,
+                      'y': 3}
+        json_string = Base.to_json_string(dictionary)
+        self.assertCountEqual(json.loads(json_string), dictionary)
 
     def test_to_json_string_none(self):
-        self.assertEqual(Base.to_json_string(None), '[]')
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_from_json_string(self):
+        string = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        json_list = Base.from_json_string(string)
+        self.assertTrue(isinstance(json_list, list))
+
+    def test_from_json_string_content(self):
+        string = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        expected_list = [{'id': 1, 'width': 10, 'height': 7, 'x': 2, 'y': 8}]
+        json_list = Base.from_json_string(string)
+        self.assertEqual(json_list, expected_list)
+
+    def test_from_json_string_none(self):
+        json_list = Base.from_json_string(None)
+        self.assertEqual(json_list, [])
+
+    def test_create_error(self):
+        with self.assertRaises(Exception) as err:
+            Base.create()
+        self.assertEqual(str(err.exception), "Wrong class") # change error msg to your own
