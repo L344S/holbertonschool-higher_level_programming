74e654e9567b1853ac86adf3d276f53b07f7a7a9
L344S <7474@holbertonstudents.com>
2024-02-21 01:40:01 +0100

    Working on tests/test_models/test_base.py (DW)
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -17,7 +17,3 @@ class TestBase(unittest.TestCase):
 
     def test_to_json_string_none(self):
         self.assertEqual(Base.to_json_string(None), '[]')
-
-
-if __name__ == '__main__':
-    unittest.main()
