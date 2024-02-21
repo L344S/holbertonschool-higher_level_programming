0df1574d0e669cd869668dfa5159f0ecdd329315
L344S <7474@holbertonstudents.com>
2024-02-21 11:03:22 +0100

    Implemented 101-locked_class.py (AT)
    LOC OK | PYCS NOW OK | GDS

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for LockedClass"""
+
+
+class LockedClass:
+    """
+    Description:
+        A class to lock the attributes of the class.
+    """
+    __slots__ = ['first_name']
