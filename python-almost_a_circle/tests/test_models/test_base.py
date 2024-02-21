cff943e7a4598beb74dd32923ea560b74b1ac169
L344S <7474@holbertonstudents.com>
2024-02-21 01:35:27 +0100

    Working on tests/test_models/test_base.py (GDS)
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Tests for the Base, Rectangle and Square classes"""
+"""Test for Base class"""
 import unittest
 from models.base import Base
 
@@ -12,5 +12,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base1.id, 1)
         base2 = Base()
         self.assertEqual(base2.id, 2)
-        base3 = Base(3)
-        self.assertEqual(base3.id, 3)
+        base3 = Base(125)
+        self.assertEqual(base3.id, 125)
+
+    def test_to_json_string_none(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
