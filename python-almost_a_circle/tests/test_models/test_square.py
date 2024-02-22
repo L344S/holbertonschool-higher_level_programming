62977b7079a8e25b945943dddcf7895de7893bb2
L344S <7474@holbertonstudents.com>
2024-02-22 20:59:13 +0100

    Implemented Nico's UT (updated version)
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
 """Tests for the Square class"""
+import os
 import unittest
 from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """Tests for the Square class"""
+
+    def test_Square_without_x_y(self):
+        square = Square(1)
+        self.assertEqual(square.size, 1)
+
+    def test_Square_without_y(self):
+        square = Square(1, 2)
+        self.assertEqual(square.size, 1)
+        self.assertEqual(square.x, 2)
+
+    def test_Square_without_id(self):
+        square = Square(1, 2, 3)
+        self.assertEqual(square.size, 1)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+
+    def test_Square_with_all(self):
+        square = Square(1, 2, 3, 4)
+        self.assertEqual(square.size, 1)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+        self.assertEqual(square.id, 4)
