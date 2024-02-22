519106b1f0859ff59caf59e99c093476fb10ae71
L344S <7474@holbertonstudents.com>
2024-02-22 21:04:57 +0100

    Implemented Nico's UT (updated version)
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -8,6 +8,7 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
     """Tests for the Square class"""
 
+    """-------- BASECASES --------"""
     def test_Square_without_x_y(self):
         square = Square(1)
         self.assertEqual(square.size, 1)
@@ -29,3 +30,16 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.x, 2)
         self.assertEqual(square.y, 3)
         self.assertEqual(square.id, 4)
+
+    """-------- TYPE ERROR --------"""
+    def test_Square_size_type(self):
+        with self.assertRaises(TypeError):
+            square = Square("1")
+
+    def test_Square_x_type(self):
+        with self.assertRaises(TypeError):
+            square = Square(1, "2")
+
+    def test_Square_y_type(self):
+        with self.assertRaises(TypeError):
+            square = Square(1, 2, "3")
