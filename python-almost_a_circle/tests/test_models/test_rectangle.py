48e6ed4dd8aeb332b3da004f4d64211cf9651384
L344S <7474@holbertonstudents.com>
2024-02-22 13:07:10 +0100

    Implemented Nico's UT (updated version)
    LOC OK | PYCS NOW OK | GDS

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Tests for the Base class"""
+import sys
 import unittest
+from io import StringIO
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -8,33 +10,33 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """Tests for the Rectangle class"""
 
-    def test_rectangle_creation_1(self):
-        rect = Rectangle(1, 2)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
+    def test_rectangle_creation_1(self):  # Base case for rectangle creation
+        rectangle = Rectangle(1, 2)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
 
-    def test_rectangle_creation_2(self):
-        rect = Rectangle(1, 2, 3)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
-        self.assertEqual(rect.x, 3)
+    def test_rectangle_creation_2(self):  # Test for width, height, x
+        rectangle = Rectangle(1, 2, 3)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
+        self.assertEqual(rectangle.x, 3)
 
-    def test_rectangle_creation_3(self):
-        rect = Rectangle(1, 2, 3, 4)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
-        self.assertEqual(rect.x, 3)
-        self.assertEqual(rect.y, 4)
+    def test_rectangle_creation_3(self):  # Test for width, height, x, y
+        rectangle = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
+        self.assertEqual(rectangle.x, 3)
+        self.assertEqual(rectangle.y, 4)
 
-    def test_rectangle_creation_4(self):
-        rect = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
-        self.assertEqual(rect.x, 3)
-        self.assertEqual(rect.y, 4)
-        self.assertEqual(rect.id, 5)
+    def test_rectangle_creation_4(self):  # Test for width, height, x, y, id
+        rectangle = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
+        self.assertEqual(rectangle.x, 3)
+        self.assertEqual(rectangle.y, 4)
+        self.assertEqual(rectangle.id, 5)
 
-    def test_type(self):
+    def test_type(self):  # Test if typeError is raised when needed
         self.assertRaises(TypeError, Rectangle, "1", 1)
         self.assertRaises(TypeError, Rectangle, width='2')
         self.assertRaises(TypeError, Rectangle, width=float('NaN'))
@@ -43,7 +45,7 @@ class TestRectangle(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 1, 1, x={})
         self.assertRaises(TypeError, Rectangle, 1, 1, y=2.5)
 
-    def test_value(self):
+    def test_value(self):  # Test if valueError is raised when needed
         self.assertRaises(ValueError, Rectangle, -5, 1)
         self.assertRaises(ValueError, Rectangle, 0, 1)
         self.assertRaises(ValueError, Rectangle, 1, 0)
@@ -52,11 +54,22 @@ class TestRectangle(unittest.TestCase):
         self.assertRaises(ValueError, Rectangle, 1, 1, -2)
         self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
 
-    def test_area(self):
-        rect_1 = Rectangle(6, 2)
-        self.assertEqual(rect_1.area(), 12)
+    def test_area(self):  # Test for area method
+        rectangle = Rectangle(6, 2)
+        self.assertEqual(rectangle.area(), 12)
 
-    def test_rectangle_str(self):
-        rect = Rectangle(1, 2, 3, 4, 5)
+    def test_rectangle_str(self):  # Test for __str__ method
+        rectangle = Rectangle(1, 2, 3, 4, 5)
         expected_str = "[Rectangle] (5) 3/4 - 1/2"
-        self.assertEqual(str(rect), expected_str)
+        self.assertEqual(str(rectangle), expected_str)
+
+    def test_display_without_x_y(self):  # Test for display method
+        rectangle = Rectangle(2, 2)
+        expected_output = "##\n##\n"
+        old_stdout = sys.stdout
+        # Redirect stdout temporarily to object StringIO
+        sys.stdout = mystdout = StringIO()
+        rectangle.display()
+        sys.stdout = old_stdout  # Put the orginal stdout back
+        # Compare the output store in StringIO object with expected output
+        self.assertEqual(mystdout.getvalue(), expected_output)
